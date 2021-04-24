5062f690a7b881e3c3f429d92716086c729d0a97
victor kitesh <victor.katile@student.moringaschool.com>
2021-04-24 14:20:51 +0300

    created local storage

--- sctipt.js
+++ sctipt.js
@@ -1,4 +1,20 @@
 //  the business logic
-function pizza(size,crust,toppings) {
-    
-}
+const form = document.querySelector('#form');
+const btnViewOrder = document.querySelector("#view-order");
+const tot = document.querySelector("#total");
+let cart = [];
+form.addEventListener("submit", function(event){
+  event.preventDefault();
+  const fd = new FormData(form);
+  let order = {};
+  for (const key of fd.keys()) {
+    if(fd.get(key).toString().length > 0){
+      order[key]  = fd.get(key).toString();
+    }
+  }
+  order["total"] = parseInt(order["qty"] * order["price"]);
+  cart.push(order);
+  if(confirm("This pizza will be added to your cart")){
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }
+});
